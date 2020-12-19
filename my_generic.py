584d22f88ef2220e04dd45e3391538b521f6f4f5
Leon_Chao <liangzhao@buaa.edu.cn>
2020-12-19 11:53:31 +0800

    modified crossover method

--- my_generic.py
+++ my_generic.py
@@ -2,31 +2,39 @@ import numpy as np
 import math
 import random
 
+from numpy.core.numeric import cross
+
 class generic():
     def __init__(self) -> None:
         super().__init__()
         self.num_population=100#随机生成的初始解的总数
-        self.copy_num=70#保留的解的个数
         self.cross_num=20#交叉解的个数
         self.var_num=10#变异解的个数
         
-    def initPopulation(self, cities : np.ndarray):
-        self.num_cities = cities.shape[0]
+    def initPopulation(self, citiesCoord : np.ndarray):
+        self.num_cities = citiesCoord.shape[0]
         # generate distance matrix
-        coord_x = cities[:,(0,)]
-        coord_y = cities[:,(1,)]
+        coord_x = citiesCoord[:,(0,)]
+        coord_y = citiesCoord[:,(1,)]
         self.dist_mat = np.sqrt((coord_x - coord_x.T)**2 + (coord_y - coord_y.T)**2)
         # initialize first generation population 
         # @population ndarray: num_cities * [fitness, cities order]
-        random_idx = np.random.permutation(np.random.arange(self.num_cities))
         self.population = np.zeros(self.num_population, self.num_cities+1)
         for i in range(self.num_population):
-            random_idx = np.random.permutation(np.random.arange(self.num_cities))
-            self.population[i, 0] = calcDist(random_idx, self.dist_mat)
+            random_idx = np.random.permutation(np.arange(self.num_cities))
+            self.population[i, 0] = calcFitness(random_idx, self.dist_mat)
             self.population[i, 1:] = random_idx
     
-    def crossOver(self):
-        pass
+    def crossOver(self, crossRatio : float):
+        assert(0<crossRatio<1)
+        crossSpan = int(crossRatio * self.num_cities)
+        children = np.zeros((self.cross_num, self.num_cities+1))
+        for i in range(self.cross_num):
+            parent_idx = np.random.choice(self.num_population, 2)
+            parent1 = self.population[parent_idx[0]]
+            parent2 = self.population[parent_idx[1]]
+            
+            child = 0
     
     def mutate(self):
         pass
@@ -35,12 +43,12 @@ class generic():
         pass
         
         
-def calcDist(cities : np.ndarray, distMat : np.ndarray) -> int:
-    assert(cities.shape[1]==distMat.shape[0]==distMat.shape[1])
-    idx1 = cities
+def calcFitness(citiesOrder : np.ndarray, distMat : np.ndarray) -> int:
+    assert(citiesOrder.shape[1]==distMat.shape[0]==distMat.shape[1])
+    idx1 = citiesOrder
     idx2 = np.concatenate(idx1[1:], idx1[0])
     dist = distMat[idx1, idx2]
-    return np.sum(dist)
+    return 10000.0/np.sum(dist)
     
 
 
